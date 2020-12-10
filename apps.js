caa65ad71335007dff3b62fd5ac4d7ada7a22692
Agnes Ko <agnes.ko68@yahoo.ca>
2020-12-10 01:11:25 -0500

    added submit button blur effect

--- apps.js
+++ apps.js
@@ -1,6 +1,6 @@
 const images = document.querySelector('#thumb-bar');
 const hiddenArea = document.querySelector('.footer')
-
+const submitBtn = document.querySelector('#button')
 
 
 document.querySelector('#label').style.display = 'none';
@@ -12,5 +12,21 @@ images.addEventListener('mouseover', e => {
         document.querySelector('#label').style.display = '';
         document.querySelector('#input').style.display = '';
         document.querySelector('#button').style.display = '';
+    }
+    changeSource = e.target.src
+    document.querySelector('.main-img').src = changeSource;
+})
+
+submitBtn.addEventListener('click', e => {
+    const inputValue = document.querySelector('#effect');
+    if (inputValue.value == 'blur') {
+        let currentImg = document.querySelector('.main-img');
+        let currentSrc = currentImg.src;
+        let newSrc = currentSrc.substring(0, currentSrc.length-4) + 'B.jpg';
+        currentImg.outerHTML = `<img class="main-img" src="${newSrc}">`;
+        inputValue.value = '';
+    } else {
+        alert('Invalid Effect')
+        inputValue.value = '';
     }
 })
